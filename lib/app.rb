4d5a4c3c80f46368c206613f1175d736e7a19d40
Yuki Nishida <rsworks.snow@gmail.com>
2015-01-03 16:19:29 +0900

    Separated functions concerning configure files from app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -9,7 +9,6 @@ require 'bundler/setup'
 
 require 'clone'
 require 'conf'
-require 'kwalify'
 require 'log'
 require 'store'
 require 'user'
@@ -21,7 +20,6 @@ class App
   end
 
   CONFIG = find_base(:config)
-  SCHEMA = CONFIG + 'schema'
   DB     = find_base(:db)
   KADAI  = DB + 'kadai'
   BUILD  = find_base(:build)
@@ -29,12 +27,6 @@ class App
   SCRIPT = find_base(:script)
 
   FILES = {
-    :master        => CONFIG + 'master.yml',
-    :master_schema => SCHEMA + 'master.yml',
-    :local         => CONFIG + 'local.yml',
-    :local_schema  => SCHEMA + 'local.yml',
-    :scheme        => CONFIG + 'scheme.yml',
-    :template      => CONFIG + 'template.yml',
     :data          => DB + 'data.yml',
     :log           => 'log.yml',
     :build         => TESTER + 'build.rb',
@@ -52,6 +44,7 @@ class App
 
   def initialize(remote_user=nil)
     @remote_user = remote_user
+    @conf = Conf.new(CONFIG)
   end
 
   def file(name)
@@ -64,44 +57,23 @@ class App
 
   def logger()
     unless @logger
-      @logger = Logger.new(conf[:logger, :path])
-      @logger.level = LOGGER_LEVEL[conf[:logger, :level]]
+      @logger = Logger.new(conf[:master, :logger, :path])
+      @logger.level = LOGGER_LEVEL[conf[:master, :logger, :level]]
     end
     return @logger
   end
 
-  def verify_yml(s)
-    def check(e, f)
-      raise e.inject("config file error: '#{f}'\n"){|a, e|
-        a += "[#{e.path}] #{e.message}\n"
-      } if e && !e.empty?
-    end
-    schema = (s.to_s + "_schema").intern
-    schema_file = file(schema)
-    check(Kwalify::MetaValidator.instance.validate(schema_file), schema)
-    check(Kwalify::Validator.new(schema_file).validate(file(s)), s)
-  end
-
   def conf()
-    unless @conf
-      verify_yml(:master)
-      @conf = Conf.new(file(:master), (file(:local) rescue nil))
-    end
     return @conf
   end
 
-  def template()
-    @template ||= Conf.new(file(:template), (file(:local) rescue nil))
-    return @template
-  end
-
   def user()
-    @user ||= conf[:user] || @remote_user || ENV['USER']
+    @user ||= conf[:master, :user] || @remote_user || ENV['USER']
     return @user
   end
 
   def su?(u=nil)
-    return conf[:su].include?(u || user)
+    return conf[:master, :su].include?(u || user)
   end
 
   def user_dir(r)
@@ -113,8 +85,8 @@ class App
       user_store = Store::YAML.new(FILES[:data])
       user_store.ro.transaction do |store|
         @users = (store['data'] || []).map{|u| User.new(u)}
-        @users.reject!{|u| u.login != user} unless conf[:record, :open] || su? || all
-        unless conf[:record, :show_login]
+        @users.reject!{|u| u.login != user} unless conf[:master, :record, :open] || su? || all
+        unless conf[:master, :record, :show_login]
           # Override User#login to hide user login name
           @users.each{|u| def u.login() return token end}
         end
@@ -160,7 +132,7 @@ class App
     optional = []
     optional << :log if option[:log]
 
-    type = (file(:scheme)['scheme'].find{|r| r['id']==id} || {})['type']
+    type = (conf[:scheme, :scheme].find{|r| r['id']==id} || {})['type']
 
     if (type == 'post') or (type == 'closed')
       fname = KADAI + id + u + FILES[:log]
@@ -183,7 +155,7 @@ class App
     when 'solved'
       return Report::Solved.new(src)
     when 'record'
-      scheme = (file(:scheme)['report'] || {})[id] || {}
+      scheme = conf[:scheme, :report, id] || {}
       return Report::Record.new(src, scheme)
     else
       return src
@@ -205,7 +177,7 @@ class App
     }
 
     checkers.each do |k,f|
-      c = conf[:post, :limit, k]
+      c = conf[:master, :post, :limit, k]
       return false if c && c != 0 && c < f[]
     end
 
